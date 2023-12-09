928e02c505a835d52a0da09cf3416e7e3f8f8f2b
Joe Heck <heckj@mac.com>
2023-12-09 12:12:48 -0800

    adding has_docs and last_activity to be able to replicate displayed search result entirely

--- Tests/SPISearchResultTests/SPISearchResultTests.swift
+++ Tests/SPISearchResultTests/SPISearchResultTests.swift
@@ -4,10 +4,22 @@ import SPISearchResult
 import XCTest
 
 final class SPISearchResultTests: XCTestCase {
-    let samplePackageResult = SearchResult.Package(id: .init(owner: "heckj", repository: "SPISearchResult"), name: "SPISearchResult", package_keywords: [], summary: "A package that defines the transfer objects to capture a collection of Search Results from Swift Package Index", stars: 1)
+    static var isoDate: Date {
+        do {
+            // earlier mechanism using DateFormatter:
+            //    let newFormatter = ISO8601DateFormatter()
+            //    formatter.formatOptions = [.withInternetDateTime]
+            //    let date = newFormatter.date(from: "2023-12-04T06:38:23Z")
+            return try Date.ISO8601FormatStyle.iso8601.parse("2023-12-04T06:38:23Z")
+        } catch {
+            return Date.now
+        }
+    }
+
+    let samplePackageResult = SearchResult.Package(id: .init(owner: "heckj", repository: "SPISearchResult"), name: "SPISearchResult", package_keywords: [], summary: "A package that defines the transfer objects to capture a collection of Search Results from Swift Package Index", stars: 1, has_docs: true, last_activity: isoDate)
 
     let expectedJSONString = """
-    {"id":{"o":"heckj","r":"SPISearchResult"},"k":[],"n":"SPISearchResult","s":"A package that defines the transfer objects to capture a collection of Search Results from Swift Package Index","x":1}
+    {"a":723364703,"d":true,"id":{"o":"heckj","r":"SPISearchResult"},"k":[],"n":"SPISearchResult","s":"A package that defines the transfer objects to capture a collection of Search Results from Swift Package Index","x":1}
     """
 
     func testJSONEncode() throws {
