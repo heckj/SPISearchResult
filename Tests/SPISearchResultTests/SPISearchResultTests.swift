34baeb131544064d4b488e2fef9fb2a92c42bf5a
Joe Heck <heckj@mac.com>
2023-12-09 12:20:48 -0800

    switch to older date creation using Formatter for Linux

--- Tests/SPISearchResultTests/SPISearchResultTests.swift
+++ Tests/SPISearchResultTests/SPISearchResultTests.swift
@@ -5,15 +5,11 @@ import XCTest
 
 final class SPISearchResultTests: XCTestCase {
     static var isoDate: Date {
-        do {
-            // earlier mechanism using DateFormatter:
-            //    let newFormatter = ISO8601DateFormatter()
-            //    formatter.formatOptions = [.withInternetDateTime]
-            //    let date = newFormatter.date(from: "2023-12-04T06:38:23Z")
-            return try Date.ISO8601FormatStyle.iso8601.parse("2023-12-04T06:38:23Z")
-        } catch {
-            return Date.now
-        }
+        let newFormatter = ISO8601DateFormatter()
+        newFormatter.formatOptions = [.withInternetDateTime]
+        return newFormatter.date(from: "2023-12-04T06:38:23Z")!
+        // Linux Foundation doesn't have Date.ISO8601FormatStyle
+        // return try Date.ISO8601FormatStyle.iso8601.parse("2023-12-04T06:38:23Z")
     }
 
     let samplePackageResult = SearchResult.Package(id: .init(owner: "heckj", repository: "SPISearchResult"), name: "SPISearchResult", package_keywords: [], summary: "A package that defines the transfer objects to capture a collection of Search Results from Swift Package Index", stars: 1, has_docs: true, last_activity: isoDate)
