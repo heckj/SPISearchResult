552e92b541d0a1ffbe167298f7cd9eab5ba1e030
Joe Heck <heckj@mac.com>
2023-12-09 12:26:22 -0800

    sticking with date encoding strategy of iso8601

--- Tests/SPISearchResultTests/SPISearchResultTests.swift
+++ Tests/SPISearchResultTests/SPISearchResultTests.swift
@@ -15,12 +15,13 @@ final class SPISearchResultTests: XCTestCase {
     let samplePackageResult = SearchResult.Package(id: .init(owner: "heckj", repository: "SPISearchResult"), name: "SPISearchResult", package_keywords: [], summary: "A package that defines the transfer objects to capture a collection of Search Results from Swift Package Index", stars: 1, has_docs: true, last_activity: isoDate)
 
     let expectedJSONString = """
-    {"a":723364703,"d":true,"id":{"o":"heckj","r":"SPISearchResult"},"k":[],"n":"SPISearchResult","s":"A package that defines the transfer objects to capture a collection of Search Results from Swift Package Index","x":1}
+    {"a":"2023-12-04T06:38:23Z","d":true,"id":{"o":"heckj","r":"SPISearchResult"},"k":[],"n":"SPISearchResult","s":"A package that defines the transfer objects to capture a collection of Search Results from Swift Package Index","x":1}
     """
 
     func testJSONEncode() throws {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .sortedKeys
+        encoder.dateEncodingStrategy = .iso8601
         let encodedPackage = try encoder.encode(samplePackageResult)
         let stringResult = try XCTUnwrap(String(data: encodedPackage, encoding: .utf8))
         // print(stringResult)
